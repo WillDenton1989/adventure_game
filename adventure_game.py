8a52cf122c701c2de6fa65c50cf9c1264c68e368
William Denton <williamdenton@Williams-MacBook-Pro.local>
2022-07-27 11:35:56 -0400

    added defense scalar, added monster name to relavent text

--- adventure_game.py
+++ adventure_game.py
@@ -8,13 +8,16 @@ player = {
     "hit_points": 9,
     "attack_power": 5,
     "defense": 1,
+    "defense_scalar": 1,
     "decision": None
     }
 
 monster = {
+    "name": "Orc",
     "hit_points": 8,
     "attack_power": 5,
     "defense": 1,
+    "defense_scalar": .5,
     "decision": None
     }
 
@@ -38,7 +41,9 @@ def user_name():
     elif player["name"] == "motherfucker":
         print("\nWhat aint no country i ever heard of. they speak english in what?")
     elif player["name"] == "boobs":
-        print("\nMmmmmmm those are nice")
+        print("\nMmmmmmm Boobs")
+    elif player["name"] == "dood":
+        print("\nSup dood :)")
     else:
         print("\nHeh funny name.")
     return player["name"]
@@ -51,9 +56,9 @@ def quit():
 def round_counter(round):
     return round + 1
 
-def monsters_choice():
+def enemy_npc_choice():
     x = randint(1, 10)
-    if(x <= 6):
+    if(x <= 7):
         return "attack"
     else:
         return "defend"
@@ -70,8 +75,8 @@ def is_someone_dead(player, monster,):
     else:
         return False
 
-def defend(original_defense, round):
-    new_defense = original_defense + 1
+def defend(original_defense, defense_scalar):
+    new_defense = original_defense + defense_scalar
     if(new_defense == 4):
         return original_defense
     else:
@@ -83,31 +88,33 @@ def attack(attack_power, hit_points, defense):
     return hit_points - attack_value
 
 # Main
-
 print("Welcome intrepid adventurer! \n\nThis is the Adventure Game!(working title, dont laugh)\n\n")
 player["name"] = user_name()
+#future return point
 print(f"\nAlright {player['name']}. lets go!")
+print(f"\n{player['name']} is fighting a monsterous {monster['name']}")
 parser.show_controls()
 
+#battle start
 while(is_someone_dead(player, monster,) == False):
     round = round_counter(round)
 
-    print(f"\n\nRound {round}: monster - {monster['hit_points']}, player - {player['hit_points']}")
-    print(f"\nplayer defense: {player['defense']}, monster defense {monster['defense']}")
+    print(f"\n\nRound {round}: {monster['name']} - {monster['hit_points']}, {player['name']} - {player['hit_points']}")
+    print(f"\nplayer defense: {player['defense']}, enemy defense {monster['defense']}")
 
     # 1) player input
     player_input_string = input(f"I await your command {player['name']}: ")
     player["decision"] = parser.parse_player_input(player_input_string)
 
     # 2) gather monster input
-    monster["decision"] = monsters_choice()
+    monster["decision"] = enemy_npc_choice()
 
     # 3) execute defends
     if(player["decision"] == "defend"):
-        player["defense"] = defend(player["defense"], round)
+        player["defense"] = defend(player["defense"], player["defense_scalar"])
 
     if(monster["decision"] == "defend"):
-        monster["defense"] = defend(monster["defense"], round)
+        monster["defense"] = defend(monster["defense"], monster["defense_scalar"])
 
     # 4) execute attacks
     if(player["decision"] == "attack"):
@@ -120,6 +127,6 @@ while(is_someone_dead(player, monster,) == False):
     if(player["decision"] == "quit"):
         quit()
 
-    print(f"\nThe player chooses to {player['decision']}, The Monster choses to {monster['decision']}")
+    print(f"\n{player['name']} chooses {player['decision']}, The {monster['name']} choses to {monster['decision']}")
 
-print(f"\n\n{player['name']} has finished the Adventure! So far...")
+print(f"\n\n{player['name']} has finished their Adventure! So far...")
