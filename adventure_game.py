6167759b7c4a1f9fd2d4d44d22a530b093f5f763
William Denton <williamdenton@Williams-MacBook-Pro.local>
2022-08-10 16:13:37 -0400

    Fixes bug in assigning player name

--- adventure_game.py
+++ adventure_game.py
@@ -11,7 +11,7 @@ import map_parser
 #game welcome menu
 print("Welcome intrepid adventurer! \n\nThis is the Adventure Game!(working title, dont laugh)\n\n")
 game_commands.player["name"] = game_commands.user_name()
-print(f"\nAlright {player['name']}. lets go!")
+print(f"\nAlright {game_commands.player['name']}. lets go!")
 #battle start
 game_functions.battle(game_commands.player, game_commands.monster, game_functions.attack, game_functions.defend, game_parser.parse_player_input, monster_module.enemy_npc_choice, game_commands.quit)
 #Continue menu loop
