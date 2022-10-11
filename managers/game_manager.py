edb52e20550d600bafc9ddab9adeb42f21a9e382
William Denton <williamdenton@williams-mbp.lan>
2022-10-11 12:28:54 -0400

    initial commit for level_parser to class

--- managers/game_manager.py
+++ managers/game_manager.py
@@ -15,7 +15,7 @@ from models.events.inventory_event import InventoryEvent
 from models.state import State
 
 class GameManager(ManagerBase):
-    """Herald ye, i am the god of this game. All shall tremeble at mine approach. My gaze pierces cloud, shadow, earth and flesh."""
+    """Herald ye, i am the god of this game. All shall tremble at mine approach. My gaze pierces cloud, shadow, earth and flesh."""
 
     def __init__(self):
         event_dispatcher = EventDispatcher()
